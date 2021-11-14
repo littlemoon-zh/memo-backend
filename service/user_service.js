ea348fac78500ab7db017df9179b709c135259ba
xiaoyuhang1 <xiaoyuhang1@yy.com>
2021-11-14 17:41:31 -0500

    fix

--- service/user_service.js
+++ service/user_service.js
@@ -5,21 +5,25 @@
 
 const User = require('../models/user');
 const {sign} = require('../middleware/jwt_verify');
+const {use} = require("express/lib/router");
 
 const user_login = async (req, res) => {
-    const {username, password} = req.body;
-    const user = await User.findOne({"username": username});
-    console.log(user);
-    if (!user) {
-        res.json({msg: 'username does not exist', code: 400, data: {}})
-        return;
-    }
-    if (user.password === password) {
-        const token = sign({username, id: String(user._id)});
-        res.json({msg: 'success', code: 200, data: {token}})
-    } else {
-        res.json({msg: 'wrong password, try again', code: 400, data: {}})
-    }
+  const {username, password} = req.body;
+  const user = await User.findOne({"username": username});
+  console.log(user);
+  if (!user) {
+    res.json({msg: 'username does not exist', code: 400, data: {}})
+    return;
+  }
+  console.log(password)
+  console.log(req.body)
+  console.log(user.password === password, user.password, password, username)
+  if (user.password === password) {
+    const token = sign({username, id: String(user._id)});
+    res.json({msg: 'success', code: 200, data: {token, username}})
+  } else {
+    res.json({msg: 'wrong password, try again', code: 400, data: {}})
+  }
 }
 
 const user_logout = (req, res) => {
@@ -27,23 +31,23 @@ const user_logout = (req, res) => {
 }
 
 const user_register = async (req, res) => {
-    try {
-        let user = req.body;
-        const filter = {username: user.username}
-        console.log(filter);
-        const result = await User.find(filter);
-        // console.log('res', result);
-        if (result.length === 0) {
-            const new_user = await User.create(user);
-            await new_user.save();
-            res.json({msg: 'success', status: 200});
-        } else {
-            res.json({msg: 'User already exists', status: 400})
-        }
-    } catch (error) {
-        console.log(error)
-        res.json({msg: 'Register failed', status: 400});
+  try {
+    let user = req.body;
+    const filter = {username: user.username}
+    console.log(filter);
+    const result = await User.find(filter);
+    // console.log('res', result);
+    if (result.length === 0) {
+      const new_user = await User.create(user);
+      await new_user.save();
+      res.json({msg: 'success', status: 200});
+    } else {
+      res.json({msg: 'User already exists', status: 400})
     }
+  } catch (error) {
+    console.log(error)
+    res.json({msg: 'Register failed', status: 400});
+  }
 
 
 }
